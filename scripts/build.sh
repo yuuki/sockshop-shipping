fd55a92a6b4a2889f4a714d57d9876161ef84e56
y_uuki <yuki.tsubo@gmail.com>
2020-09-01 10:41:34 +0900

    Add stress-ng to image

--- scripts/build.sh
+++ scripts/build.sh
@@ -26,7 +26,7 @@ CODE_DIR=$(cd $SCRIPT_DIR/..; pwd)
 echo $CODE_DIR
 $DOCKER_CMD run --rm -v $HOME/.m2:/root/.m2 -v $CODE_DIR:/usr/src/mymaven -w /usr/src/mymaven maven:3.2-jdk-8 mvn -DskipTests package
 
-cp $CODE_DIR/target/*.jar $CODE_DIR/docker/$(basename $CODE_DIR)
+cp $CODE_DIR/target/*.jar $CODE_DIR/docker/shipping
 
 for m in ./docker/*/; do
     REPO=${GROUP}/$(basename $m)
